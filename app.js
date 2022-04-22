7115c2ef844db1f905518c6a73f8c4b56f189393
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-04-22 12:30:24 -0500

    added in start of percentage calculator for ORM

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-// countdown timer for fun
-var countDownDate = new Date("Jan 25, 2023 15:37:25").getTime();
+// percent calculator
+// function percentage(num, per) {
+//     return (num / 100) * per;
+// }
 
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-      }
-    }, 1000);
+function caluculatePercentage() {
+    var num = document.getElementById("onerepmax").value;
+    var per = document.getElementById("per").value;
+    var result = (num / 100) * per;
+    document.getElementById("result").innerHTML = result;
+}
